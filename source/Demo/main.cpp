db67a284c31bff15e336314365953b4b08358f23
liuyx-e <1614553756@qq.com>
2020-01-12 17:06:34 +0800

    测试：通过增加一个顶点颜色属性来改变显示的颜色

--- source/Demo/main.cpp
+++ source/Demo/main.cpp
@@ -32,10 +32,10 @@ int main()
 
 	unsigned int VAO, VBO, EBO;
 	float vertices[] = {
-		-0.5,-0.5,0,
-		0.5,-0.5,0,
-		0.5,0.5,0,
-		-0.5,0.5,0
+		-0.5,-0.5,0,	1,0,0,
+		0.5,-0.5,0,		0,1,0,
+		0.5,0.5,0,		0,0,1,
+		-0.5,0.5,0,		1,1,0
 	};
 
 	unsigned int indices[] = {
@@ -54,8 +54,10 @@ int main()
 	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
 	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
 
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), static_cast<void*>(0));
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), reinterpret_cast<void*>(0));
 	glEnableVertexAttribArray(0);
+	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), reinterpret_cast<void*>(3 * sizeof(float)));
+	glEnableVertexAttribArray(1);
 
 	glBindBuffer(GL_ARRAY_BUFFER, 0);
 	glBindVertexArray(0);
