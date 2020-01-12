8fa052f672c4954e66ff28f8a24312e3d4247bf0
liuyx-e <1614553756@qq.com>
2020-01-12 17:58:31 +0800

    增加顶点结构体

--- source/Demo/main.cpp
+++ source/Demo/main.cpp
@@ -7,6 +7,7 @@
 #include "Program.h"
 #include <thread>
 #include <chrono>
+#include "Vertex.h"
 
 GLFWwindow* GetMainWindow();
 void framebuffer_size_callback(GLFWwindow* window, int width, int height);
@@ -31,11 +32,11 @@ int main()
 	}
 
 	unsigned int VAO, VBO, EBO;
-	float vertices[] = {
-		-0.5,-0.5,0,	1,0,0,
-		0.5,-0.5,0,		0,1,0,
-		0.5,0.5,0,		0,0,1,
-		-0.5,0.5,0,		1,1,0
+	Vertex vertices[] = {
+		Vertex(glm::vec3(-0.5,-0.5,0),glm::vec3(1,0,0)),
+		Vertex(glm::vec3(0.5,-0.5,0),glm::vec3(0,1,0)),
+		Vertex(glm::vec3(0.5,0.5,0),glm::vec3(0,0,1)),
+		Vertex(glm::vec3(-0.5,0.5,0),glm::vec3(1,1,0)),
 	};
 
 	unsigned int indices[] = {
@@ -54,17 +55,16 @@ int main()
 	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
 	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
 
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), reinterpret_cast<void*>(0));
-	glEnableVertexAttribArray(0);
-	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), reinterpret_cast<void*>(3 * sizeof(float)));
-	glEnableVertexAttribArray(1);
+	glVertexAttribPointer(Vertex::VertexAttributeLocation::Position, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex), reinterpret_cast<void*>(offsetof(Vertex,position)));
+	glEnableVertexAttribArray(Vertex::VertexAttributeLocation::Position);
+	glVertexAttribPointer(Vertex::VertexAttributeLocation::Color, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex), reinterpret_cast<void*>(offsetof(Vertex, color)));
+	glEnableVertexAttribArray(Vertex::VertexAttributeLocation::Color);
 
 	glBindBuffer(GL_ARRAY_BUFFER, 0);
 	glBindVertexArray(0);
 	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
 
 	Program* defaultProgram = ProgramFactory::Get(ProgramType::Default);
-
 	while (!glfwWindowShouldClose(window))
 	{
 		processInput(window);
--- source/Demo/shaders/default.vert
+++ source/Demo/shaders/default.vert
@@ -1,11 +1,12 @@
 #version 330 core
-layout(location = 0) in vec3 aPos;
-layout(location = 1) in vec3 aColor;
+layout(location = 0) in vec3 vPos;
+layout(location = 1) in vec3 vNormal;
+layout(location = 2) in vec3 vColor;
 
 out vec3 fColor;
 
 void main()
 {
-	fColor = aColor;
-	gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);
+	fColor = vColor;
+	gl_Position = vec4(vPos.x, vPos.y, vPos.z, 1.0);
 }
